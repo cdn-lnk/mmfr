de7cfd2dc430d61ac3cc683c3c667ccc57986eff
Nino <77451637+cdn-lnk@users.noreply.github.com>
2024-10-06 17:41:06 +0200

    Add plot for clean amps

---
+++ plot/AC30.py
@@ -0,0 +1,15 @@
+from matplotlib.pyplot import loglog as plot
+from .__fft__ import fft_wrapper
+
+list_amp = ["blue"]
+list_eq = ["blue", "green", "white", "yellow", "red"]
+list_effects = ["white"]
+list_modify = ["violet"]
+
+for amp in list_amp:
+	for eq in list_eq:
+		for effect in list_effects:
+			for modify in list_modify:
+				name = f"{amp}-{eq}-{effect}-{modify}"
+				x, y = fft_wrapper(name)
+				plot(x, y, color=eq, linewidth=.2, label=name)
