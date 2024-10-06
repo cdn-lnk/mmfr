21a94dfb388ea27e4eeb053ac1a86d2aca2b1127
Nino <77451637+cdn-lnk@users.noreply.github.com>
2024-10-06 11:04:53 +0200

    Use flac to compress data

--- data/acquire.sh
+++ data/acquire.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env sh
 # 4194304 = 2²² samples, which is approx. 95 seconds at 44100 Hz
-arecord -D "hw:1,0" -f cd -s 4194304 "$1"
+arecord -D "hw:1,0" -f cd -s 4194304 "$1.wav"
+flac --best "$1.wav"
+rm "$1.wav"
