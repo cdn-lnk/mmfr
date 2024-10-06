4ac1241c17365e878a947a3c98500d7ce4f5e22d
Nino <77451637+cdn-lnk@users.noreply.github.com>
2024-10-06 21:51:56 +0200

    Use long options for arecord

--- acquire.sh
+++ acquire.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
 # 4194304 = 2²² samples, which is approx. 95 seconds at 44100 Hz
-arecord -D "hw:1,0" -f cd -s 4194304 "data/$1.wav"
+arecord --device "hw:1,0" --format cd --samples 4194304 "data/$1.wav"
 flac --best "data/$1.wav"
 rm "data/$1.wav"
